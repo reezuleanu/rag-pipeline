768a4a5e6887272321109557cc32379da9716f3a
reezuleanu <reezuleanu@icloud.com>
2025-06-05 22:25:53 +0300

    BasePipeline Modifications

--- rag_pipeline/pipelines/base.py
+++ rag_pipeline/pipelines/base.py
@@ -2,14 +2,20 @@ from abc import ABC, abstractmethod
 from uuid import UUID, uuid4
 import logging
 
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    Document,
+    StorageContext,
+)
+from llama_index.core.vector_stores.types import BasePydanticVectorStore
 
 
 class BasePipeline(ABC):
     """Pipeline blueprint"""
 
     def __init__(self, vector_store):
-        self.vector_store = vector_store
+        self.vector_store: BasePydanticVectorStore = vector_store
         self.logger = logging.getLogger(__name__)
 
     @abstractmethod
@@ -21,9 +27,8 @@ class BasePipeline(ABC):
         """
         raise NotImplementedError
 
-    @abstractmethod
-    def ingest_pdf(self, pdf_path: str) -> UUID:
-        """Ingest the pdf into a vector store and returns its id
+    def ingest_pdf(self, pdf_path: str, file_id: UUID = None) -> UUID:
+        """Ingest the pdf into the vector store and returns its id
 
         Args:
             pdf_path (str): path to the pdf to ingest
@@ -31,9 +36,51 @@ class BasePipeline(ABC):
         Returns:
             UUID: id of the ingested pdf
         """
-        raise NotImplementedError
 
-    def run(self, return_ids: bool = False) -> UUID | None:
+        file_id = file_id or uuid4()
+
+        documents = SimpleDirectoryReader(input_files=[pdf_path]).load_data()
+
+        for d in documents:
+            d.metadata["file_id"] = file_id
+
+        documents = self.process_llamaindex_documents(documents, pdf_path, file_id)
+
+        VectorStoreIndex.from_documents(
+            documents,
+            storage_context=StorageContext.from_defaults(
+                vector_store=self.vector_store
+            ),
+        )
+
+        self.on_pdf_ingested(pdf_path, file_id)
+
+    def on_finish(self) -> None:
+        """A method to wrap up the pipeline if needed"""
+        pass
+
+    def on_pdf_ingested(self, pdf_path, file_id) -> None:
+        """A method for allowing logic after every pdf document ingestion"""
+        pass
+
+    def process_llamaindex_documents(
+        self,
+        documents: list[Document],
+        pdf_path: str,
+        file_id: UUID,
+    ) -> list[Document]:
+        """Method to handle custom document parsing logic
+
+        Args:
+            documents (list[Document]): llamaindex documents
+
+        Returns:
+            list[Document]: llamaindex documents after modifications/custom logic
+        """
+
+        return documents
+
+    def run(self, return_ids: bool = False, *args, **kwargs) -> UUID | None:
         """Run the pipeline
 
         Args:
@@ -44,13 +91,15 @@ class BasePipeline(ABC):
         """
         ingested_ids = []
 
-        pdfs = self.acquire_pdfs()
+        pdfs = self.acquire_pdfs(*args, **kwargs)
 
         for pdf in pdfs:
             vector_store_id = self.ingest_pdf(pdf)
             ingested_ids.append(vector_store_id)
 
         self.logger.info(f"Successfully ingested {len(ingested_ids)} documents!")
+
+        self.on_finish()
 
         if return_ids:
             return ingested_ids
