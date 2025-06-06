be1487d256ed139949d036d9c55168999c300c2a
reezuleanu <reezuleanu@icloud.com>
2025-06-06 19:16:14 +0300

    Modified settings, updated demo app

--- rag_pipeline/demo/app.py
+++ rag_pipeline/demo/app.py
@@ -12,21 +12,6 @@ from rag_pipeline.settings import settings
 # keep index in memory
 index = None
 
-# get username and hashed password from env
-credentials = {"usernames": {settings.USERNAME: {"password": settings.PASSWORD}}}
-
-authenticator = stauth.Authenticate(
-    credentials,
-    "rag_pipeline_cookie",
-    "rag_pipeline",
-)
-
-if auth_tuple := authenticator.login("main"):
-    # print(auth_tuple)
-    pass
-
-authenticator.logout(location="sidebar")
-
 
 def init_index():
     global index
@@ -45,39 +30,54 @@ def init_index():
     return index
 
 
-def render_chat():
-    for role, message in st.session_state.chat_history:
-        with st.chat_message(role):
-            st.markdown(message)
+# get username and hashed password from env
+credentials = {
+    "usernames": {settings.APP_USERNAME: {"password": settings.APP_PASSWORD}}
+}
+
+authenticator = stauth.Authenticate(
+    credentials,
+    "rag_pipeline_cookie",
+    "rag_pipeline",
+)
 
 
-st.title("🦙 RAG demo with LlamaIndex")
+if st.session_state.get("authentication_status") is None:
+    authenticator.login("main")
 
-if "chat_engine" not in st.session_state:
-    llm = OpenAI(
-        # model="gpt-4o",
-        temperature=0.1,
-        streaming=True,
-    )
-    index = init_index()
-    st.session_state.chat_engine = index.as_chat_engine(
-        chat_mode="best", llm=llm, verbose=True
-    )
-    st.session_state.chat_history = [("ai", "Hi, how can i help you?")]
+elif st.session_state.get("authentication_status") is False:
+    authenticator.login("main")
+    st.error("Username/password is incorrect")
+
+elif st.session_state.get("authentication_status"):
+
+    def render_chat():
+        for role, message in st.session_state.chat_history:
+            with st.chat_message(role):
+                st.markdown(message)
+
+    st.title("🦙 RAG demo with LlamaIndex")
+
+    if "chat_engine" not in st.session_state:
+        llm = OpenAI(
+            model=settings.OPENAI_MODEL,
+            temperature=settings.LLM_TEMPERATURE,
+            streaming=True,
+        )
+        index = init_index()
+        st.session_state.chat_engine = index.as_chat_engine(
+            chat_mode="best", llm=llm, verbose=True
+        )
+        st.session_state.chat_history = [("ai", "Hi, how can i help you?")]
     render_chat()
 
-if not st.session_state.get("is_generating", False):
     prompt = st.chat_input("Have any questions?")
-else:
-    prompt = st.chat_input("The bot is answering...", disabled=True)
 
-
-if prompt:
-    st.session_state.is_generating = True
-    st.session_state.chat_history.append(("user", prompt))
-    render_chat()
-    response = st.session_state.chat_engine.stream_chat(prompt)
-    with st.chat_message("ai"):
-        final_response = st.write_stream(response.response_gen)
-    st.session_state.chat_history.append(("ai", str(final_response)))
-    st.session_state.is_generating = False
+    if prompt:
+        st.session_state.chat_history.append(("user", prompt))
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        response = st.session_state.chat_engine.stream_chat(prompt)
+        with st.chat_message("ai"):
+            final_response = st.write_stream(response.response_gen)
+        st.session_state.chat_history.append(("ai", str(final_response)))
--- rag_pipeline/settings.py
+++ rag_pipeline/settings.py
@@ -3,8 +3,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     OPENAI_API_KEY: str = ""
-    USERNAME: str = "admin"
-    PASSWORD: str = ""
+    OPENAI_MODEL: str = "gpt-4o-mini"
+    LLM_TEMPERATURE: float = 0.1
+
+    APP_USERNAME: str = "admin"
+    APP_PASSWORD: str = ""
 
     OPENSEARCH_ENDPOINT: str = "https://localhost:9200"
     OPENSEARCH_USERNAME: str = "admin"
