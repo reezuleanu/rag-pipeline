a834047d1f67c892fb0f9c6814a9bd718e258e76
reezuleanu <reezuleanu@icloud.com>
2025-06-06 19:25:06 +0300

    Modified compose and start script

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 mkdir -p ./opensearch
 
-export USERNAME="admin"
-export PASSWORD=""
+export APP_USERNAME="admin"
+export APP_PASSWORD=""
+
+export OPENAI_API_KEY=""
+export OPENAI_MODEL="gpt-4o-mini"
+export LLM_TEMPERATURE="0.1"
+
 export OPENSEARCH_PASSWORD=""
+export OPENSEARCH_ENDPOINT="https://opensearch-rag-pipeline:9200"
+export OPENSEARCH_USERNAME="admin"
+export OPENSEARCH_PASSWORD=""
+export OPENSEARCH_INDEX="rag_example_index"
 
 docker-compose up -d
